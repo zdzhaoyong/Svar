30ff81f98216efe5e525f8dd0b6b26ac218c52db
zhaoyong <zd5945@126.com>
2019-04-25 09:57:57 +0800

    update basic.py

--- python/python/basic.py
+++ python/python/basic.py
@@ -2,11 +2,17 @@ import svar
 
 basic=svar.loadSvarPlugin("/data/zhaoyong/Program/Apps/Tests/JSVar/python/build/libbasic.so")
 
-print("basic type ")
-print("context")
-
 pyobj={"i":1,"d":1.2,"s":"hello world","v":[1,2,3]}
 print("pyobj is:",pyobj)
+
+ret=basic.obtainInfo(pyobj)
+print("ret is:",ret)
+
+print(basic.dtos(100.423))
+
+c=basic.add(34,3)
+print(c)
+print(basic.add(c,3))
 
 
 #robj=basic.obtainInfo(pyobj)
