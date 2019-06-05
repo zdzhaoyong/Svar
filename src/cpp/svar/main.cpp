1fa6de8a9856d82cd1954bd622ab64a6e1480c6c
zhaoyong <zd5945@126.com>
2019-06-05 13:38:55 +0800

    confirm get a ref and change is not thread safe

--- src/cpp/svar/main.cpp
+++ src/cpp/svar/main.cpp
@@ -372,22 +372,32 @@ TEST(Svar,Alias){
 }
 
 TEST(Svar,Thread){
-    auto readThread=[](){
+    auto doubleThread=[](){
         while(!svar.get<bool>("shouldstop",false)){
-            double& d=svar.get<double>("thread.Double",100);
+            double d=svar.get<double>("thread.Double",100);
+            svar.set<double>("thread.Double",d++);
+
+            // double& d=svar.get<double>("thread.Double",100); d++;
+            // The above line is dangerous in Mac system since the value may set after memory free
+
+            Svar v=svar.get("thread.Double",100);
+            v=100;// this is much safer to set a copy of svar instead the raw pointer
+
             GSLAM::Rate::sleep(1e-4);
         }
     };
-    auto writeThread=[](){
-        svar.set<double>("thread.Double",svar.GetDouble("thread.Double")++);
+    auto intThread=[](){
         while(!svar.get<bool>("shouldstop",false)){
-            svar.set<int>("thread.Double",10);// use int instead of double to violately testing robustness
+            // use int instead of double to violately testing robustness
+            int i=svar.get<int>("thread.Double",10);
+            svar.set<int>("thread.Double",i);
+
             GSLAM::Rate::sleep(1e-4);
         }
     };
     std::vector<std::thread> threads;
-    for(int i=0;i<svar.GetInt("readThreads",4);i++) threads.push_back(std::thread(readThread));
-    for(int i=0;i<svar.GetInt("writeThreads",4);i++) threads.push_back(std::thread(writeThread));
+    for(int i=0;i<svar.GetInt("doubleThreads",4);i++) threads.push_back(std::thread(doubleThread));
+    for(int i=0;i<svar.GetInt("intThreads",4);i++) threads.push_back(std::thread(intThread));
 
     GSLAM::Rate::sleep(1);
     svar.set("shouldstop",true);
