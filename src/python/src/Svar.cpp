101d4b7d041329fc3130b22f001008d7d52dd32f
zhaoyong <zd5945@126.com>
2019-07-10 20:42:06 +0800

    use Registry in Python

--- src/python/src/Svar.cpp
+++ src/python/src/Svar.cpp
@@ -1,26 +1,11 @@
 #define __SVAR_BUILDIN__
 #include <Svar/SvarPy.h>
+#include <Svar/Registry.h>
 
 using namespace GSLAM;
 
 PyObject* load(std::string pluginPath){
-    SharedLibraryPtr plugin(new SharedLibrary(pluginPath));
-    if(!plugin->isLoaded()){
-        LOG(ERROR)<<"Unable to load plugin "<<pluginPath;
-        return Py_None;
-    }
-
-    GSLAM::Svar* (*getInst)()=(GSLAM::Svar* (*)())plugin->getSymbol("svarInstance");
-    if(!getInst){
-        LOG(ERROR)<<"No svarInstance found in "<<pluginPath<<std::endl;
-        return Py_None;
-    }
-    GSLAM::Svar* inst=getInst();
-    if(!inst){
-        LOG(ERROR)<<"svarInstance returned null.\n";
-        return Py_None;
-    }
-    return SvarPy::getModule(*inst);
+    return SvarPy::getModule(Registry::load(pluginPath));
 }
 
 SVAR_PYTHON_IMPL(svar)
